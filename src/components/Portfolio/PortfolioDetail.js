da9f2ef388ad2d7e013e6c5aa208d2a891bc22bd
Sakib12345 <sakibalislam12345@gmail.com>
2021-07-05 17:25:10 +0600

    design added

--- src/components/Portfolio/PortfolioDetail.js
+++ src/components/Portfolio/PortfolioDetail.js
@@ -6,9 +6,9 @@ const PortfolioDetail = (props) => {
     return (
         <div className='col-md-4 mt-5'>
             <div className='d-flex justify-content-center'>
-                <div className='text-center mb-3'>
+                <div className='text-center mb-3 p-3 card shadow-lg bg-body rounded'>
                 <iframe width="420" title="chaya" height="220" src={videoLink} ></iframe>
-                <h4 className="defaultFontTheme mt-2" style={{fontSize: '30px', color:'#d67738'}}>{name}</h4>
+                <h4 className="defaultFontTheme pt-4" style={{fontSize: '30px', color:'#d67738'}}>{name}</h4>
                 </div>
             </div>
         </div>
--- src/components/Services/ServiceDetails.js
+++ src/components/Services/ServiceDetails.js
@@ -6,7 +6,7 @@ const ServiceDetails = (props) => {
     return (
         <div className='col-md-4 mt-5'>
             <div className='d-flex justify-content-center'>
-                <div className='text-center mb-5'>
+                <div className='text-center mb-5 p-3 card shadow-lg bg-dark rounded w-75' >
                     <i class={icon} style={{ fontSize: '60px', color: '#F8E6E6' }}></i>
                     <h3 className="defaultFontTheme mt-2" style={{ fontSize: '30px', color: '#F8E6E6' }}>{name}</h3>
                     <h4 style={{ fontSize: '20px', color: '#F8E6E6' }}>Price: {price} BDT</h4>
