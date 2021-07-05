4e42393fb4aa2221168a548aed8f76b64c6c3d00
Sakib12345 <sakibalislam12345@gmail.com>
2021-07-05 17:35:10 +0600

    fix portfolio column issues video

--- src/components/Portfolio/PortfolioDetail.js
+++ src/components/Portfolio/PortfolioDetail.js
@@ -7,7 +7,7 @@ const PortfolioDetail = (props) => {
         <div className='col-md-3 mt-lg-5 mt-3 mb-3 mb-lg-5'>
             <div className='d-flex justify-content-center'>
                 <div className='text-center mb-3 p-lg-2 p-0 card shadow-lg bg-body rounded'>
-                <iframe width="320" title="chaya" height="160" src={videoLink} ></iframe>
+                <iframe width="360" title="chaya" height="160" src={videoLink} ></iframe>
                 <h4 className="defaultFontTheme pt-lg-2 pt-2" style={{fontSize: '25px', color:'#d67738'}}>{name}</h4>
                 </div>
             </div>
